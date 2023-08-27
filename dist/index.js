a621623e773c1013d1b067731835233e40334df2
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-08-27 10:01:31 +0600

    sdf

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _index = _interopRequireDefault(require("./src/components/TableNavigator/index"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = {
+  TableSlider: _index["default"]
+};
+exports["default"] = _default;
